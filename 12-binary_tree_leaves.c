dd3f0b706e9a4bfec098a9dbd1c2b003f3f9c826
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-15 07:28:54 -0800

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,19 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: a pointer to root node of tree to measure size
- * Return: size of binary tree
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: a pointer to the root node of the tree
+ * to count the number of leaves
+ * Return: no. of leaves in binary tree
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t lsize, rsize;
+	size_t lleaf, rleaf;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	lsize = binary_tree_size(tree->left);
-	rsize = binary_tree_size(tree->right);
-	return (lsize + rsize + 1);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		lleaf = binary_tree_leaves(tree->left);
+		rleaf = binary_tree_leaves(tree->right);
+		return (lleaf + rleaf);
+	}
 }
