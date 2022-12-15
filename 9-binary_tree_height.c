189ffddc52c8a1adcb36ca1eb50dbd5fc8170587
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-15 07:49:00 -0800

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,1 +1,44 @@
+#include "binary_trees.h"
 
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height
+ * Return: height of the binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int rows = traverse_rows(tree);
+
+	if (rows == 0)
+	{
+		return (0);
+	}
+
+	return (rows - 1);
+}
+/**
+ * traverse_rows - counts rows of tree
+ * @tree: tree to be evaluated
+ *
+ * Return: rows, or 0 on failure
+ */
+int traverse_rows(const binary_tree_t *tree)
+{
+	int left;
+	int right;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left = traverse_rows(tree->left);
+	right = traverse_rows(tree->right);
+
+	if (left > right)
+	{
+		return (left + 1);
+	}
+
+	return (right + 1);
+}
